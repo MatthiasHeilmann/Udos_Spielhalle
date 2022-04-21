e0d4add90679cc65107f2661352d013477ab82bf
Ynfiniti <sean_schwarz@gmx.de>
2022-04-21 14:28:30 +0200

    implementing socket.io

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const http = require('http')
 const server = http.createServer(app);
 const port = 8080;
-
+const io = require("socket.io")(server);
+// To store all sockets that are currently connected to this servers io
+const sockets = {};
 // Starting webserver
 server.listen(port, function(){
     console.log('Webserver läuft auf Port %d', port);
@@ -16,6 +18,11 @@ app.use(function timeLog(req, res, next) {
     next();
 });
 
+/**
+ * TODO Learn https://stackoverflow.com/questions/29446462/nodejs-how-to-update-client-after-external-program-has-returned
+ * For socket.io.
+ * Lässt den server mit einem client verbinden. Antworten zu jeder zeit ohne gebraucht von fetch o.ä.
+ */
 app.get('/' , (req, res) => {
     res.sendFile(__dirname + '/main.html');
 });
@@ -24,14 +31,41 @@ app.get('/schiffeversenken', (req, res) => {
     res.sendFile(__dirname + '/games/schiffeversenken.html');
 });
 
-app.get('/svGetOpenGames', (req, res) => {
+io.on('connection', (socket) => {
+    let isHost;
+    sockets[socket.id] = {socket, connectedSocket: null};
+    console.log("User connected: " + socket.id);
 
-});
+    socket.on('disconnect', () => {
+        console.log("User disconnected: " + socket.id);
+    });
 
-app.put('/svCreateOpenGame', (req, res) => {
+    socket.on('svSetState', (state) => {
+        isHost = (state === 'host');
+        console.log(socket.id + " set state to " + state + " (" + isHost + ")");
+    })
 
-});
+    socket.on('svSocketTest', (text) => {
+        console.log("Got new connection!: " + text);
+    });
 
-app.post('/svConnectToGame', (req, res) => {
+    socket.on('svCreateOpenGame', (hostId) => {
 
-});
+    });
+
+    socket.on('svGetOpenGames', () => {
+
+    });
+
+    socket.on('svAskJoinGame', (hostId) => {
+
+    });
+
+    socket.on('svAnswerGameRequest', (clientId, allowJoin) => {
+
+    });
+
+    socket.on('svJoinGame', (hostId) => {
+
+    });
+})
